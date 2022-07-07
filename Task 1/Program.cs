cebb8523656b68896c5b4326587bbddbe0b61a4f
maxgavaga <ferrarif138@yandex.ru>
2022-07-07 12:55:54 +0300

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+
+ int[,] array = new int[4, 4];
+ FillArray(array);
+ PrintArray(array);
+ Console.WriteLine();
+ SortArray(array);
+ PrintArray(array);
+
+void FillArray(int[,] arr)
+ {
+     for (int m = 0; m < arr.GetLength(0); m++)
+     {
+         for (int n = 0; n < arr.GetLength(1); n++)
+         {
+             arr[m, n] = new Random().Next(-10, 10);
+         }
+     }
+ }
+void PrintArray(int[,] arr)
+ {
+     for (int m = 0; m < arr.GetLength(0); m++)
+     {
+         for (int n = 0; n < arr.GetLength(1); n++)
+         {
+             Console.Write($"{arr[m, n]}|");
+         }
+         Console.WriteLine();
+     }
+ }
+void SortArray(int[,] arr)
+ {
+     for (int m = 0; m < arr.GetLength(0); m++)
+     {
+         for (int n = 0; n < arr.GetLength(1); n++)
+         {
+             for (int k = 0; k < arr.GetLength(1) - n - 1; k++)
+             {
+                 if (arr[m, k] < arr[m, k + 1])
+                 {
+                     int t = arr[m, k];
+                     arr[m, k] = arr[m, k + 1];
+                     arr[m, k + 1] = t;
+                 }
+             }
+         }
+     }
+ }
